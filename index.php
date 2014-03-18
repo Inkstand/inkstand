df7b9569188139c36a2dce7325d72ee8af6b4cd6
Joe Conradt <joeconradt@wi.rr.com>
2014-03-18 05:29:04 +0000

    Add a bunch of content and stlying to home page

--- index.php
+++ index.php
@@ -24,18 +24,30 @@ require_once DIR . '/plugin/theme/foundation/layout/header.php';
 <div id="view-content">
 
 	<div class="module module2"> 
-	
-	<?php
-
-	
-
-	?>
-
+		<h1 class="big center"><span>portfolio of</span> Joe Conradt</h1>
+		<div class="content">
+			<hr>
+			<p class="center">Programmer&nbsp;&nbsp;<i class="fa fa-code"></i>&nbsp;&nbsp;Web Developer&nbsp;&nbsp;<i class="fa fa-code"></i>&nbsp;&nbsp;Designer</p>
+			<h2>Hi there!</h2>
+			<p>I'm a guy with a laptop and too many ideas; utilizaing my left and right brain powers. Check out my <a href='index.php/article'>projects</a>!</p> 
+		</div>
 	</div> 
 
 	<div class="module module1">
-		<h3>Welcome!</h3>
-		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sagittis fermentum tellus, vitae iaculis lorem vestibulum a. Suspendisse lobortis dui tristique odio cursus, ut condimentum nisi lacinia.</p>
+		<div class="content">
+			<img class="profile" src="<?php echo WWW . '/plugin/theme/foundation/image/joe.jpg'; ?>">
+		</div>
+		<footer>
+			<div class="width33 fa-container">
+				<i class="fa fa-phone fa-2x"></i>
+			</div>
+			<div class="width33 fa-container">
+				<i class="fa fa-linkedin-square fa-2x"></i>
+			</div>
+			<div class="width33 fa-container">
+				<i class="fa fa-file-text-o fa-2x"></i>
+			</div>
+		</footer>
 	</div>
 
 	<div class="module module1">
--- plugin/theme/foundation/layout/header.php
+++ plugin/theme/foundation/layout/header.php
@@ -3,11 +3,15 @@
 <head>
 	<title>Joe Conradt</title>
 	<link rel="stylesheet" type="text/css" href="<?php echo WWW . '/plugin/theme/foundation/css/style.css'; ?>">
+	<link rel="stylesheet" type="text/css" href="<?php echo WWW . '/core/lib/fontawesome/css/font-awesome.css'; ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 
 <body>
 
 	<div id="view-header">
-
+		<ul>
+			<li>HOME</li>
+			<li>PROJECTS</li>
+		</ul>
 	</div>
