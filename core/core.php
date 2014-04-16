c33a86f151bd2f7d6c65937b8e411c31affb3f9c
Joe Conradt <joeconradt@wi.rr.com>
2014-04-15 22:03:34 -0500

    bootstrap navigation menu started

--- core/core.php
+++ core/core.php
@@ -399,6 +399,42 @@ class Core
 			}
 		}
 	}
+
+	/* function to print a menu */
+	public function menu($menuid) {
+
+		// this will be a very versatile function, allowing the echoing of links, search boxes, dropdowns, etc. 
+		// Bootstrap!
+
+		$table = $this->getTableFormat("menu_items");
+		$menuitems = DB::query("SELECT * FROM $table WHERE menuid = %i", $menuid);
+
+		// echo some HTML
+
+		echo '<nav class="navbar navbar-default" role="navigation">';
+		echo 	'<div class="container-fluid">';
+		echo 		'<div class="navbar-header">
+				      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+				        <span class="sr-only">Toggle navigation</span>
+				        <span class="icon-bar"></span>
+				        <span class="icon-bar"></span>
+				        <span class="icon-bar"></span>
+				      </button>
+				      <a class="navbar-brand" href="' . WWW . '">Home</a>
+				    </div>';
+
+		echo 	'<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">';
+
+		// loop through all the menu elements 
+		foreach ($menuitems as $menuitem) {
+			
+		}
+
+		echo	'</div><!-- /.navbar-collapse -->
+			  </div><!-- /.container-fluid -->
+			</nav>';
+
+	}
 }
 
 ?>
