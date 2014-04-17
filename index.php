1b341eed4cfb8afa350f390b02f8cbc8c02a342f
Jack <rattles7@hotmail.com>
2014-04-17 16:08:00 -0500

    Permalinks (no index.php)

--- index.php
+++ index.php
@@ -5,12 +5,38 @@ require_once DIR . '/core/route/route.class.php';
 require_once 'core/lib/addTinymce.php';
 
 // *** route ***
+//echo WWW;
+$pageURL = 'http';
+ if (isset($_SERVER["HTTPS"]) && $_SERVER["HTTPS"] == "on") {$pageURL .= "s";}
+ $pageURL .= "://";
+ if ($_SERVER["SERVER_PORT"] != "80") {
+  $pageURL .= $_SERVER["SERVER_NAME"].":".$_SERVER["SERVER_PORT"].$_SERVER["REQUEST_URI"];
+ } else {
+  $pageURL .= $_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"];
+ }
+ //echo "<br>" . $pageURL;
+ $custom_routepath = "";
+
+ if (WWW . "/" != $pageURL && WWW . "/index.php" != $pageURL) {
+ 	//echo "custom page";
+ 	$custom_routepath = str_replace(WWW, '', $pageURL) . "/";
+ 	//echo "cutstom route is   " . $custom_routepath;
+ } else {
+ 	//echo "regular page";
+ }
+
 
 if(isset($_SERVER['PATH_INFO'])) {
 	$routepath = $_SERVER['PATH_INFO'];
-	
+
 	$route = new Route();
 	$route->directRoute($routepath);
+
+	die();
+
+} else if ($custom_routepath != "") {
+	$route = new Route();
+	$route->directRoute($custom_routepath);
 
 	die();
 
