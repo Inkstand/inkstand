2ea1f836be95b11f46d9f56d78a4ed5660f89e55
Joe Conradt <joeconradt@wi.rr.com>
2014-04-17 14:06:48 -0500

    style changes

---
+++ plugin/theme/admin/config.php
@@ -0,0 +1,14 @@
+<?php 
+
+require_once '../../../config.php';
+require_once DIR . '/plugin/theme/theme.php';
+
+class AdminTheme extends Theme {
+
+	public $theme_layouts = array();
+	public $parent = 'foundation';
+
+}
+
+
+?>
