788be66a87bbd245fa1445c5d10a07797e599063
Joe Conradt <joeconradt@wi.rr.com>
2014-04-18 21:23:36 -0500

    header fix

--- plugin/theme/bootstrap/layout/header.php
+++ plugin/theme/bootstrap/layout/header.php
@@ -30,10 +30,7 @@
 
 	<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
 
-<<<<<<< HEAD
-	<div id="view-header">
-=======
+
 	<div id="header">
->>>>>>> bootstrap
 		<?php $CORE->menu(1) ?>
 	</div>
