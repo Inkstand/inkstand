24a91ddf53ed7d119ffeab6888d8e47d3ffab78f
Joe Conradt <joeconradt@wi.rr.com>
2014-04-18 20:07:36 -0500

    Temporary fix

--- plugin/theme/foundation/layout/default.php
+++ plugin/theme/foundation/layout/default.php
@@ -6,7 +6,7 @@ require_once 'header.php';
 
 <div id="view-content">
 
-	<div class="module module2"> 
+	<div class="module module3"> 
 	
 	<?php
 
@@ -15,22 +15,6 @@ require_once 'header.php';
 	?>
 
 	</div> 
-
-	<div class="module module1">
-		<div class="content">
-			<h3>Welcome!</h3>
-			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sagittis fermentum tellus, vitae iaculis lorem vestibulum a. Suspendisse lobortis dui tristique odio cursus, ut condimentum nisi lacinia.</p>
-		</div>
-	</div>
-
-	<div class="module module1">
-		<div class="content">
-			<h3>Menu</h3>
-			<ul>
-				<li>Menu logic soon</li>
-			</ul>
-		</div>
-	</div>
 
 </div>
 
