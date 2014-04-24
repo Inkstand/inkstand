d9cdc43b9b05fd7bd796ede408ec8939e1f55f00
Joe Conradt <joeconradt@wi.rr.com>
2014-04-24 14:01:26 -0500

    Page component sql file updates

--- components/page/page.sql.php
+++ components/page/page.sql.php
@@ -5,11 +5,13 @@ require_once 'config.php';
 $table = $CORE->getTableFormat("page");
 DB::query("CREATE TABLE IF NOT EXISTS $table (
   `id` INT(7) NOT NULL AUTO_INCREMENT,
-  `title` TINYTEXT,
-  `description` TEXT,
-  `content` TEXT,
-  `menuid` INT,
-  `permissionid` INT,
+  `title` tinytext,
+  `description` text,
+  `content` text,
+  `datecreated` int(11) NOT NULL,
+  `datemodified` int(11) NOT NULL,
+  `usercreatedid` int(11) NOT NULL,
+  `usermodifiedid` int(11) NOT NULL,
   PRIMARY KEY  (`id`)
 );");
 
