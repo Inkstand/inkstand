9ee7889bdc4e8cc2f11c10a4695940fb65ed427c
Jack <rattles7@hotmail.com>
2014-04-27 21:58:11 -0500

    Adds code view to tinymce init

--- core/core.php
+++ core/core.php
@@ -136,7 +136,7 @@ class Core
 			plugins: [
 			"advlist autolink link image lists charmap print preview hr anchor pagebreak",
 			"searchreplace wordcount visualblocks visualchars insertdatetime media nonbreaking",
-			"table contextmenu directionality emoticons paste textcolor responsivefilemanager"
+			"table contextmenu directionality emoticons paste textcolor responsivefilemanager code"
 			],
 			toolbar1: "undo redo | bold italic underline | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | styleselect",
 			toolbar2: "| responsivefilemanager | link unlink anchor | image media | forecolor backcolor  | print preview code ",
